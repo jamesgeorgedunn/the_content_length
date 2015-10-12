688a36d73be6c1cfd52c4387dfe19ab0e4f82737
James George Dunn <james@jamesgeorgedunn.com>
2015-10-12 14:32:23 +0100

    Initial commit.

---
+++ the_content_length.php
@@ -0,0 +1,19 @@
+<?php
+	// Adjust the length of the WordPress content you would like to show
+	// the_content_length(360, "...");
+	function the_content_length($length, $trail) {
+		$theContent = apply_filters("the_content", get_the_content());
+		$theContent = str_replace("]]>", "]]&gt;", $theContent);
+		
+		if(strlen($theContent) < $length) {
+			echo $theContent;
+		} else {
+			// Checks to see if there's a value for $trail
+			if(!empty($trail)) {
+				echo trim(substr($theContent, 0, $length)) . $trail;
+			} else {
+				echo trim(substr($theContent, 0, $length)) . "...";
+			}
+		}
+	}
+?>
